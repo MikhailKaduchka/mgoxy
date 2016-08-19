7fac6d27f48ae1013d09662e8ae3bac692da6187
Mikhail.Kaduchka <Mikhail.Kaduchka@billing.ru>
2016-08-19 16:46:44 +0300

    UI improvements

--- src/mgoxy/mgoxy.go
+++ src/mgoxy/mgoxy.go
@@ -3,68 +3,80 @@ package main
 //Simple proxy application
 
 import (
-  "fmt"
-  "net/http"
-  "os"
-  "bufio"
-  "log"
+	"fmt"
+	"net/http"
+	"os"
+	"bufio"
+	"log"
 )
 
 func main() {
 
-  http.HandleFunc("/", indexHandler)
-  http.HandleFunc("/index.html", indexHandler)
-  http.HandleFunc("/go/", urlHandler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/go/", urlHandler)
 
-  port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 
-  http.ListenAndServe(":" + port, nil)
+	//If port not set then use default port 8080
+	if len(port) == 0 {
+		port = "8080"
+	}
 
-  fmt.Println("Proxy started on port: ", port)
+	http.ListenAndServe(":" + port, nil)
+
+	fmt.Println("Proxy started on port: ", port)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, templateStr)
+	fmt.Fprintf(w, templateStr)
 }
 
 func urlHandler(w http.ResponseWriter, r *http.Request) {
 
-  url := r.URL.Path[len("/go/"):]
+	url := r.URL.Path[len("/go/"):]
 
-  resp, err := http.Get("http://" + url)
-  if err != nil {
-    log.Println("Couldn't reach url. Error: ", err)
-  }
-  defer resp.Body.Close()
+	params := r.URL.RawQuery
 
-  reader := bufio.NewReader(resp.Body)
+	askPageUrl := "http://" + url
 
-  readPage := true
+	if len(params) > 0 {
+		askPageUrl += "?" + params
+	}
 
-  for readPage {
+	resp, err := http.Get(askPageUrl)
+	if err != nil {
+		panic("Couldn't reach url \" " + askPageUrl + "\":" + err.Error())
+	}
+	defer resp.Body.Close()
 
-    bytes := make([]byte, BUF_SIZE, BUF_SIZE)
+	reader := bufio.NewReader(resp.Body)
 
-    for i := 0; i < BUF_SIZE; i++ {
-      line, err := reader.ReadByte()
-      if err == nil {
-        bytes[i] = line
-      } else {
-        log.Println("Reading error: ", err)
-        //Stop loading data
-        readPage = false
-        break
-      }
-    }
+	readPage := true
 
-    w.Write(bytes)
+	for readPage {
 
-    if f, ok := w.(http.Flusher); ok {
-      f.Flush()
-    } else {
-      log.Println("Damn, no flush");
-    }
-  }
+		bytes := make([]byte, BUF_SIZE, BUF_SIZE)
+
+		for i := 0; i < BUF_SIZE; i++ {
+			line, err := reader.ReadByte()
+			if err == nil {
+				bytes[i] = line
+			} else {
+				readPage = false
+				break
+			}
+		}
+
+		w.Write(bytes)
+
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+		} else {
+			log.Println("Damn, no flush");
+		}
+	}
+
+	log.Println("Got page \"" + askPageUrl + "\" from " + r.RemoteAddr)
 }
 
 const BUF_SIZE = 512
@@ -72,7 +84,7 @@ const BUF_SIZE = 512
 const templateStr = `
 <html>
 <head>
-<title>PROXYGO</title>
+<title>Mini Go Proxy</title>
 <script type="text/javascript">
     function redirect () {
         location.href = '/go/' + document.getElementById("url").value;
@@ -80,8 +92,10 @@ const templateStr = `
 </script>
 </head>
 <body>
-<input id="url" type="text"/>
+    <div id="content" style="width:400px; margin:0 auto;">
+<input id="url" type="text" size="50" required="">
 <button id="launch" onclick="redirect();">go</button>
+  </div>
 </body>
 </html>
 `
